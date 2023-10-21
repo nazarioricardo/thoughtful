52ccfeb22fc44f06a0de8106c71377f9fa5428a8
Ricardo Nazario <nazarioricardo@gmail.com>
2023-10-21 16:01:20 -0400

    She works

--- config-overrides.js
+++ config-overrides.js
@@ -1,25 +1,25 @@
-const { override } = require('customize-cra')
+const { override } = require("customize-cra");
 
 const overrideEntry = (config) => {
   config.entry = {
-    main: './src/popup', // the extension UI
-    background: './src/background',
-    content: './src/content',
-  }
+    main: "./src/index", // the extension UI
+    background: "./src/background",
+    content: "./src/content",
+  };
 
-  return config
-}
+  return config;
+};
 
 const overrideOutput = (config) => {
   config.output = {
     ...config.output,
-    filename: 'static/js/[name].js',
-    chunkFilename: 'static/js/[name].js',
-  }
+    filename: "static/js/[name].js",
+    chunkFilename: "static/js/[name].js",
+  };
 
-  return config
-}
+  return config;
+};
 
 module.exports = {
   webpack: (config) => override(overrideEntry, overrideOutput)(config),
-}
+};
