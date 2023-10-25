99acb260a3b571a4730b4da4517aca273d3a4a56
Ricardo Nazario <nazarioricardo@gmail.com>
2023-10-25 13:02:52 -0400

    Delete alt ui

--- src/components/Alternates/AlternateItem/index.tsx
+++ src/components/Alternates/AlternateItem/index.tsx
@@ -1,15 +1,46 @@
-import { Link } from "@mui/joy";
+import { Box, Divider, Grid, IconButton, Link, Sheet } from "@mui/joy";
 import { getHostName } from "../../../constants";
+import { Delete } from "@mui/icons-material";
 
 type AlternateItemProps = {
   alternate: string;
+  isEditing: boolean;
 };
 
-function AlternateItem({ alternate }: AlternateItemProps) {
+function AlternateItem({ alternate, isEditing }: AlternateItemProps) {
   return (
-    <Link href={alternate} level="title-lg">
-      {getHostName(alternate)}
-    </Link>
+    <Sheet
+      sx={{
+        width: "100%",
+        height: 36,
+      }}
+      variant={isEditing ? "soft" : "plain"}
+    >
+      <Grid
+        container
+        sx={{
+          width: "100%",
+          height: 36,
+        }}
+        flexDirection={"row"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        position={"relative"}
+      >
+        <Grid>
+          <Link href={alternate} level="title-lg" disabled={isEditing}>
+            {getHostName(alternate)}
+          </Link>
+        </Grid>
+        {isEditing && (
+          <Grid position={"absolute"} right={0}>
+            <IconButton sx={{ borderRadius: 0 }}>
+              <Delete />
+            </IconButton>
+          </Grid>
+        )}
+      </Grid>
+    </Sheet>
   );
 }
 
--- src/components/Alternates/AlternatesList/index.tsx
+++ src/components/Alternates/AlternatesList/index.tsx
@@ -1,15 +1,51 @@
-import { Stack } from "@mui/joy";
+import { Box, Divider, Grid, IconButton, Stack } from "@mui/joy";
 import AlternateItem from "../AlternateItem";
+import { Cancel, Check, Edit, Girl } from "@mui/icons-material";
+import { useState } from "react";
 
 type AlternatesListProps = {
   alternates: string[];
 };
 
 function AlternatesList({ alternates }: AlternatesListProps) {
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+
+  const togglEdit = () => {
+    setIsEditing(!isEditing);
+  };
+
   return (
-    <Stack>
+    <Stack sx={{ minWidth: 330 }} alignItems={"center"}>
+      <Grid
+        container
+        sx={{ width: "100%", maxHeight: 36 }}
+        flexDirection={"row"}
+        justifyContent={"space-between"}
+      >
+        <Grid>
+          <IconButton
+            disabled={!isEditing}
+            color="primary"
+            sx={{ borderRadius: 0 }}
+          >
+            {isEditing && <Check />}
+          </IconButton>
+        </Grid>
+        <Grid>
+          <IconButton onClick={togglEdit} sx={{ borderRadius: 0 }}>
+            {isEditing ? <Cancel /> : <Edit />}
+          </IconButton>
+        </Grid>
+      </Grid>
       {alternates.map((alternate: string) => (
-        <AlternateItem key={alternate} alternate={alternate} />
+        <>
+          <AlternateItem
+            key={alternate}
+            alternate={alternate}
+            isEditing={isEditing}
+          />
+          <Divider sx={{ opacity: isEditing ? 1 : 0 }} />
+        </>
       ))}
     </Stack>
   );
