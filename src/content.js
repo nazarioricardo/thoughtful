9297e13459115c65967e3bc2fcca6f55712fe1a7
Ricardo Nazario <nazarioricardo@gmail.com>
2023-11-09 14:10:44 -0500

    Made draggable

--- src/content.js
+++ src/content.js
@@ -2,8 +2,16 @@ import { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import Browser from "webextension-polyfill";
 import { getFromStorage } from "./utils";
-import { Card, CssVarsProvider, Grid, IconButton, Typography } from "@mui/joy";
-import { Check } from "@mui/icons-material";
+import {
+  Card,
+  CardContent,
+  CssVarsProvider,
+  Grid,
+  IconButton,
+  Typography,
+} from "@mui/joy";
+import { Check, DragIndicator } from "@mui/icons-material";
+import Draggable from "react-draggable";
 
 function Main() {
   const [message, setMessage] = useState("");
@@ -36,38 +44,64 @@ function Main() {
 
   return (
     <CssVarsProvider defaultMode="dark">
-      <Card
-        variant="soft"
-        sx={{
-          position: "fixed",
-          top: 24,
-          right: 24,
-          zIndex: 10000,
-          width: "34vh",
-          maxWidth: "400px",
-          boxShadow: "black 4px 2px 20px 0px",
-          backdropFilter: "blur(2px)",
-          background: "#171a1ce8",
-          backgroundColor: "none",
-        }}
-      >
-        <Typography level="title-md">Remember...</Typography>
-        <Grid
-          container
-          flexDirection={"row"}
-          alignItems={"center"}
-          justifyContent={"space-between"}
+      <Draggable>
+        <Card
+          variant="soft"
+          sx={{
+            position: "fixed",
+            top: 24,
+            right: 24,
+            zIndex: 10000,
+            width: "34vh",
+            maxWidth: "400px",
+            boxShadow: "black 4px 2px 20px 0px",
+            backdropFilter: "blur(2px)",
+            background: "#171a1ce8",
+            backgroundColor: "none",
+
+            ":hover": { cursor: "grab" },
+            ":active": { cursor: "grabbing" },
+          }}
         >
-          <Grid>
-            <Typography level="title-lg">{message}</Typography>
-          </Grid>
-          <Grid>
-            <IconButton onClick={handleDone} variant="plain" color="primary">
-              <Check />
-            </IconButton>
-          </Grid>
-        </Grid>
-      </Card>
+          <CardContent>
+            <Grid
+              container
+              flex={1}
+              flexDirection={"row"}
+              alignItems={"center"}
+            >
+              <Grid
+                paddingRight={"10px"}
+                container
+                flexDirection={"column"}
+                justifyContent={"flex-end"}
+                alignItems={"center"}
+                flex={1}
+              >
+                <DragIndicator />
+              </Grid>
+              <Grid flexDirection={"column"} flex={8}>
+                <Typography level="title-md">Remember...</Typography>
+                <Typography level="title-lg">{message}</Typography>
+              </Grid>
+              <Grid
+                container
+                flexDirection={"column"}
+                justifyContent={"flex-end"}
+                alignItems={"center"}
+              >
+                <IconButton
+                  onClick={handleDone}
+                  variant="plain"
+                  color="primary"
+                >
+                  <Check />
+                </IconButton>
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+      </Draggable>
     </CssVarsProvider>
   );
 }
